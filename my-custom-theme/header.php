5678b4eec89d865839c8aac3aff3a174926867a9
Basil <basiltaliaz@gmail.com>
2025-04-06 11:51:21 -0400

    build-fina

--- my-custom-theme/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php bloginfo('name'); ?><?php wp_title('|', true, 'left'); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
