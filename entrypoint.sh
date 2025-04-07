a6375be15482ad489dd83dc762f16a42bbcb3965
Basil <basiltaliaz@gmail.com>
2025-04-07 15:00:20 -0400

    Last Try

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,13 +26,17 @@ fi
 # Update wp-config.php with the database credentials
 echo "🛠 Updating wp-config.php with database credentials..."
 
-# Add database definitions right after the opening comment block
-sed -i '/* @package WordPress/a\
+# Only add database definitions if they don't already exist
+if ! grep -q "define.*DB_NAME" /var/www/html/wp-config.php; then
+  # Insert database credentials right after the opening comment block closes with */
+  sed -i '/\*\//a\
+\
 // ** MySQL settings - You can get this info from your web host ** //\
 define( '\''DB_NAME'\'', '\'''"$WORDPRESS_DB_NAME"'\''\'' );\
 define( '\''DB_USER'\'', '\'''"$WORDPRESS_DB_USER"'\''\'' );\
 define( '\''DB_PASSWORD'\'', '\'''"$WORDPRESS_DB_PASSWORD"'\''\'' );\
 define( '\''DB_HOST'\'', '\'''"$WORDPRESS_DB_HOST"'\''\'' );' /var/www/html/wp-config.php
+fi
 
 # Set permissions
 chown www-data:www-data /var/www/html/wp-config.php
