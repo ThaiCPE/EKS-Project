929fbe2da25c390fb3815ba974a815d0caf6d624
Basil <basiltaliaz@gmail.com>
2025-04-08 00:06:30 -0400

    Last Try-2048

--- wp-config.php
+++ wp-config.php
@@ -42,3 +42,9 @@ if ( !defined('ABSPATH') )
     define('ABSPATH', dirname(_FILE_) . '/');
 // ** Sets up WordPress vars and included files. ** //
 require_once(ABSPATH . 'wp-settings.php');
+
+#define('FORCE_SSL_ADMIN', true);
+#define('FORCE_SSL', true);
+#if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+#    $_SERVER['HTTPS'] = 'on';
+#}
